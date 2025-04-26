23e9952813afefd00ce5f264a69f8c71677f54b3
Tugrul Topuz <tugrultopuz@gmail.com>
2025-04-27 00:28:02 +0300

    no numa & tensorrt

--- .github/actions/tf-serving-jetson/entrypoint.sh
+++ .github/actions/tf-serving-jetson/entrypoint.sh
@@ -6,16 +6,19 @@ git config --global --add safe.directory /github/workspace
 export TF_NEED_CUDA=1
 export TF_CUDA_VERSION=10.2
 export TF_CUDNN_VERSION=8
+export TF_TENSORRT_VERSION=8
 export TF_CUDA_COMPUTE_CAPABILITIES="5.3"
 export CUDA_HOME=/usr/local/cuda
 
 bazel build \
   --config=cuda \
   --define=no_nccl_support=true \
+  --define=with_numa_support=false \
   --define=maxrregcount=80 \
   --action_env=PYTHON_BIN_PATH="$(which python3)" \
   --action_env=TF_CUDA_VERSION="$TF_CUDA_VERSION" \
   --action_env=TF_CUDNN_VERSION="$TF_CUDNN_VERSION" \
+  --action_env=TF_TENSORRT_VERSION="$TF_TENSORRT_VERSION" \
   --action_env=TF_CUDA_COMPUTE_CAPABILITIES="$TF_CUDA_COMPUTE_CAPABILITIES" \
   //tensorflow_serving/model_servers:tensorflow_model_server
 
